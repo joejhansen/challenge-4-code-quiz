6e452b312d32debda3ca4deeca3e5335791d3882
joejhansen <josephjameshansen@gmail.com>
2022-07-28 17:08:59 -0700

    saving before class

--- develop/script.js
+++ develop/script.js
@@ -4,77 +4,86 @@ var timerText = document.querySelector("#timerCount")
 var buttonsArea = document.querySelector("#buttonsDiv")
 var h1El = document.querySelector("#headerOneText")
 var questionArea = document.querySelector("#pDescriptionText")
-var timer = 0
+var footerArea = document.querySelector('footer')
+var footerTextArea = document.createElement('h2')
+var timer = 100
+var score = 0
 
 var questionOne = {
-    question: "This is the first question?",
+    titleText: "Question 1",
+    question: "What does DOM stand for?",
     false1: {
-        answerText: "answerfalseOne",
+        answerText: "Direct Object Modifier",
         isTrue: false,
     },
     false2: {
-        answerText: "answerflasetwo",
+        answerText: "Delinear Orientation Method",
         isTrue: false,
     },
     false3: {
-        answerText: "answerfalsethree",
+        answerText: "Deli Or Market",
         isTrue: false,
     },
     correct: {
-        answerText: "This is the right one!",
+        answerText: "Document Object Model",
         isTrue: true,
     }
 }
 
 
 var questionTwo = {
-    question: "This is the second question?",
+    titleText: "Question 2",
+    question: "Which option creates an empty array?",
     false1: {
-        answerText: "answerfalseOne",
+        answerText: "function array('');",
         isTrue: false,
     },
     false2: {
-        answerText: "answerflasetwo",
+        answerText: "var array = { };",
         isTrue: false,
     },
     false3: {
-        answerText: "answerfalsethree",
+        answerText: "var array;",
         isTrue: false,
     },
     correct: {
-        answerText: "This is the right one!",
+        answerText: "var array = [ ];",
         isTrue: true,
     }
 }
 
 
 var questionThree = {
-    question: "This is the third question?",
+    titleText: "Question 3",
+    question: "Where and in which element should you link your javascript in HTML?",
     false1: {
-        answerText: "answerfalseOne",
+        answerText: "Immediately after the <html> tag",
         isTrue: false,
     },
     false2: {
-        answerText: "answerflasetwo",
+        answerText: "Anywhere, it doesn't matter",
         isTrue: false,
     },
     false3: {
-        answerText: "answerfalsethree",
+        answerText: "At the beginning of the <body> tag",
         isTrue: false,
     },
     correct: {
-        answerText: "This is the right one!",
+        answerText: "At the very end of the <body> tag",
         isTrue: true,
     }
 }
-var allAnswered
-var allTheQuestions
-var questionOneCorrect
-var questionTwoCorrect
-var questionThreeCorrect
+var allAnswered = false
+
+var allTheQuestions = {
+    questionOne,
+    questionTwo,
+    questionThree,
+}
+
 
 var buttonOne = document.createElement("li")
-buttonOne.setAttribute("class", "buttons thisOne whiteText")
+buttonOne.setAttribute("class", "buttons whiteText")
 var buttonTwo = document.createElement("li")
 buttonTwo.setAttribute("class", "buttons whiteText")
 var buttonThree = document.createElement("li")
@@ -82,61 +91,169 @@ buttonThree.setAttribute("class", "buttons whiteText")
 var buttonFour = document.createElement("li")
 buttonFour.setAttribute("class", "buttons whiteText")
 
+allTheButtons = {
+    buttonOne,
+    buttonTwo,
+    buttonThree,
+    buttonFour,
+}
+
 highScoreBtn.addEventListener('click', function (event) {
     event.preventDefault();
 })
 
+// idk what I'm doing with this, i only know array.every
+function allAreEqual(object) {
+    return new set(Object.values(object)).size === 1;
+}
 
+var timerTimer
 
 function startTimer() {
-    startBtn.setAttribute("class", "hidden")
-    var timerTimer = setInterval(function () {
-        timer++;
-        timerText.textContent = timer;
-        if (allAnswered) {
+    timer = 100
+    startBtn.classList.add('hidden')
+    timerTimer = setInterval(function () {
+        if (score > 3) {
             clearInterval(timerTimer);
-            return
+        } else if (timer < 1) {
+            clearInterval(timerTimer);
+            h1El.textContent = "You Lose!"
+            questionArea.textContent = "Sorry"
+            buttonOne.classList.add('hidden')
+            buttonTwo.classList.add('hidden')
+            buttonThree.classList.add('hidden')
+            buttonFour.classList.add('hidden')
+            startBtn.classList.remove('hidden')
         };
+        timer--;
+        timerText.textContent = timer;
     }, 1000);
 
 }
 
-function questionTwoGo(){
-    h1El.textContent = "Question 2"
-    questionArea.textContent = questionOne.question;
+function addButtons() {
     buttonsArea.append(buttonOne);
-    buttonOne.textContent = questionOne.correct.answerText
     buttonsArea.append(buttonTwo);
-    buttonTwo.textContent = questionOne.false1.answerText
     buttonsArea.append(buttonThree);
-    buttonThree.textContent = questionOne.false2.answerText
     buttonsArea.append(buttonFour);
+    buttonOne.classList.remove('hidden')
+    buttonTwo.classList.remove('hidden')
+    buttonThree.classList.remove('hidden')
+    buttonFour.classList.remove('hidden')
+    return
+}
+function gameEndWin() {
+    console.log("End of game")
+    console.log("Your score is " + timer)
+    h1El.textContent = "You win!"
+    questionArea.textContent = "Your score is " + timer
+    buttonOne.classList.add('hidden')
+    buttonTwo.classList.add('hidden')
+    buttonThree.classList.add('hidden')
+    buttonFour.classList.add('hidden')
+    startBtn.classList.remove('hidden')
+    return
+}
+
+
+function questionThreeGo() {
+    h1El.textContent = questionThree.titleText
+    questionArea.textContent = questionThree.question;
+    buttonFour.textContent = questionThree.correct.answerText
+    buttonFour.classList.remove('notThis')
+    buttonFour.classList.add('thisOne');
+    buttonFour.classList.remove('incorrect')
+    buttonOne.textContent = questionThree.false1.answerText
+    buttonOne.classList.remove('thisOne')
+    buttonOne.classList.add('notThis');
+    buttonOne.classList.remove('incorrect')
+    buttonTwo.textContent = questionThree.false2.answerText
+    buttonTwo.classList.remove('thisOne')
+    buttonTwo.classList.add('notThis');
+    buttonTwo.classList.remove('incorrect')
+    buttonThree.textContent = questionThree.false3.answerText
+    buttonThree.classList.remove('thisOne')
+    buttonThree.classList.add('notThis');
+    buttonThree.classList.remove('incorrect')
+    buttonsArea.addEventListener('click', function (event) {
+        console.log(event)
+        if (event.target.matches('.thisOne')) {
+            score++
+            gameEndWin();
+            return
+        } else if (event.target.matches('.notThis')) {
+            event.target.classList.add('incorrect')
+            console.log("wrong");
+            console.log(allAnswered)
+        }
+    })
+    return
+}
+
+function questionTwoGo() {
+    h1El.textContent = questionTwo.titleText
+    questionArea.textContent = questionTwo.question;
+    buttonThree.textContent = questionTwo.correct.answerText
+    buttonThree.classList.remove('notThis')
+    buttonThree.classList.add('thisOne');
+    buttonThree.classList.remove('incorrect')
+    buttonOne.textContent = questionTwo.false1.answerText
+    buttonOne.classList.remove('thisOne')
+    buttonOne.classList.add('notThis');
+    buttonOne.classList.remove('incorrect')
+    buttonTwo.textContent = questionTwo.false2.answerText
+    buttonTwo.classList.remove('thisOne')
+    buttonTwo.classList.add('notThis');
+    buttonTwo.classList.remove('incorrect')
     buttonFour.textContent = questionTwo.false3.answerText
+    buttonFour.classList.add('notThis');
+    buttonFour.classList.remove('incorrect')
+    buttonsArea.addEventListener('click', function (event) {
+        console.log(event)
+        if (event.target.matches('.thisOne')) {
+            score++
+            questionThreeGo();
+            return
+        } else if (event.target.matches('.notThis')) {
+            event.target.classList.add('incorrect')
+            console.log("wrong");
+            console.log(allAnswered)
+        }
+    })
+    return
 }
 
 function questionOneGo() {
-    h1El.textContent = "Question 1"
+    h1El.textContent = questionOne.titleText
     questionArea.textContent = questionOne.question;
-    buttonsArea.append(buttonOne);
-    buttonOne.textContent = questionOne.correct.answerText
-    buttonsArea.append(buttonTwo);
-    buttonTwo.textContent = questionOne.false1.answerText
-    buttonsArea.append(buttonThree);
+    buttonTwo.textContent = questionOne.correct.answerText
+    buttonTwo.classList.remove('notThis')
+    buttonTwo.classList.add('thisOne');
+    buttonOne.textContent = questionOne.false1.answerText
+    buttonOne.classList.remove('thisOne')
+    buttonOne.classList.add('notThis');
     buttonThree.textContent = questionOne.false2.answerText
-    buttonsArea.append(buttonFour);
-    buttonFour.textContent = questionTwo.false3.answerText
-    buttonsArea.addEventListener('click',function(event){
+    buttonThree.classList.remove('thisOne')
+    buttonThree.classList.add('notThis');
+    buttonFour.textContent = questionOne.false3.answerText
+    buttonFour.classList.remove('thisOne')
+    buttonFour.classList.add('notThis');
+    buttonsArea.addEventListener('click', function (event) {
         console.log(event)
-        if(event.target.matches('.thisOne')){
+        if (event.target.matches('.thisOne')) {
+            score++
             questionTwoGo();
-        }else{
-            console.log("Wrong!")
-            timer = timer + 5
+            return
+        } else if (event.target.matches('.notThis')) {
+            event.target.classList.add('incorrect')
+            console.log("wrong");
+            timer = timer - 5;
+            console.log(allAnswered)
             return
         }
     })
+    return
 }
-
 
 
 // function getRandomQuestion() {
@@ -144,10 +261,16 @@ function questionOneGo() {
 // }
 
 function startGame() {
+    score = 0
     startTimer();
+    addButtons();
     questionOneGo();
     console.log(questionOne)
+    return
 }
 
 startBtn.addEventListener('click', startGame);
 
+// look up indexOf() for relative DOM stuff for the buttons
+// use for loops to iterate the questions as li's into the questionsArea
+// do makeTheButtons() then the questions() as different functions
